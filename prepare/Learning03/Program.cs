7a3860f64f920201feb66e7cd68a5f3536b229a6
esdrono <77854186+esdrono@users.noreply.github.com>
2023-09-30 10:55:55 -0600

    Update Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class Program
+namespace Learning03
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            // Testing the constructors
+            Fraction f1 = new Fraction();
+            Fraction f2 = new Fraction(5);
+            Fraction f3 = new Fraction(3, 4);
+            Fraction f4 = new Fraction(1, 3);
+
+            // Displaying the fractions and their decimal values
+            Console.WriteLine($"{f1.GetFractionString()}\n{f1.GetDecimalValue()}");
+            Console.WriteLine($"{f2.GetFractionString()}\n{f2.GetDecimalValue()}");
+            Console.WriteLine($"{f3.GetFractionString()}\n{f3.GetDecimalValue()}");
+            Console.WriteLine($"{f4.GetFractionString()}\n{f4.GetDecimalValue()}");
+        }
     }
 }
